4c1aaaee3a6a142ce22cd7520c203c7837299ec5
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-10-05 17:36:48 +0300

    change neovim theme to kanagawa

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,23 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "kanagawa-wave",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      --  Remove bold
+      statementStyle = { bold = false },
+      overrides = function(colors)
+        return {
+          Boolean = { bold = false },
+          ["@lsp.mod.readonly.typescript"] = { link = "@lsp" },
+          ["@lsp.typemod.function.readonly"] = { bold = false },
+          ["@keyword.operator"] = { bold = false },
+          ["@string.escape"] = { bold = false },
+        }
+      end,
     },
   },
 }
--- bashrc.sh
+++ bashrc.sh
@@ -5,11 +5,12 @@
 # fi
 
 # Prompt styling
-#KANAGAWA_STYLE="\[\033[1m\033[38;2;126;156;216m\]"
-TOKYONIGHT_STYLE="\[\033[1m\033[38;2;158;206;106m\]"
+KANAGAWA_WAVE_STYLE="\[\033[1m\033[38;2;228;104;118m\]"
+KANAGAWA_DRAGON_STYLE="\[\033[1m\033[38;2;196;116;110m\]"
+TOKYONIGHT_NIGHT_STYLE="\[\033[1m\033[38;2;158;206;106m\]"
 RESET="\[\033[0m\]"
 
-PS1="${TOKYONIGHT_STYLE}${debian_chroot:+($debian_chroot)}\u@\h${RESET}:\w\$ "
+PS1="${KANAGAWA_WAVE_STYLE}${debian_chroot:+($debian_chroot)}\u@\h${RESET}:\w\$ "
 
 # Alias for dotfiles git repo
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
