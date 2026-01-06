1355f4f0050260f0624de59ce608628b418c4db4
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-01-06 12:51:03 +0200

    install css lsp automatically

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,20 @@
 return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "css-lsp",
+      })
+    end,
+  },
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
       servers = {
+        css_ls = {},
+        html = {},
         emmet_language_server = {},
       },
     },
