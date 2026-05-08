83e8da34d01b4e0f3d38e1eeb71dd26f4e7932f5
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-05-08 11:51:38 +0300

    configure blink

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -6,9 +6,23 @@ return {
         preset = "super-tab",
       },
       completion = {
+        accept = {
+          auto_brackets = {
+            enabled = false,
+          },
+        },
         trigger = {
-          show_in_snippet = false,
+          -- show_in_snippet = false,
           -- show_on_accept_on_trigger_character = false,
+        },
+      },
+      sources = {
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+            score_offset = 100,
+          },
         },
       },
     },
